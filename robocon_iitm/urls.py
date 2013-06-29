79f627ce4f2c760610df6f7f4d9f8e1a7141f1e5
AbdealiJK <abdealikothari@gmail.com>
2013-06-29 17:18:50 +0530

    Basic template added - no tweaks made yet. All dependencies handled

--- robocon_iitm/urls.py
+++ robocon_iitm/urls.py
@@ -6,12 +6,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'robocon_iitm.views.home', name='home'),
     # url(r'^robocon_iitm/', include('robocon_iitm.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', 'robocon_iitm.views.home', name='home'),
+
 )
--- robocon_iitm/views.py
+++ robocon_iitm/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import render_to_response, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 import datetime
+
+def test(request):
+    now=datetime.datetime.now()
+    html = " <center><h1>Home page<br/></h1>Its %s now</center> " % now
+    return HttpResponse(html)
+    
 def home(request):
-	now=datetime.datetime.now()
-	html="<center><h1>Home page<br/></h1>Its %s now</center>" % now
-	return HttpResponse(html)
+    return render_to_response ('base.html', {}, context_instance=RequestContext(request))
